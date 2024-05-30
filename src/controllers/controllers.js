0cbcf5712409717a6e6f6a1870e874d1089fe264
pgm-tysowils <tysowils@student.arteveldehs.be>
2024-05-30 13:41:37 +0200

    username preview added to settings page

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -4,8 +4,9 @@ import jwt from "jsonwebtoken";
 import { jwtDecode } from "jwt-decode";
 
 export async function getUser(req, res) {
-  const foundUser = await user.query().findOne("id", req.body.id);
-  return (res.body.foundUser = foundUser);
+  const decoded = jwtDecode(req.cookies.token);
+  const foundUser = await user.query().findOne("id", decoded.id);
+  res.send(foundUser);
 }
 
 export async function getUserStats(req, res) {
