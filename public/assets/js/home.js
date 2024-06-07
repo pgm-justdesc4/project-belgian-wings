158b66ca3b9d82c197cf7ec2f5aad27215cc8b2b
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2024-06-07 23:37:11 +0200

    Added xpBar interaction

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -17,21 +17,19 @@ async function activeAvatar() {
   XP BAR FOR USER 
   =================*/
   function calculateX2(levelPercentage) {
-    // The x2 value at 1% level is 300 and at 99% level is 400
-    // So the range of x2 is 100 (400 - 300)
-    // We map the level percentage (from 1 to 99) to this range (300 to 400)
-    return 300 + (levelPercentage - 1) * (100 / 98);
+    // The x2 value at 0% level is 100 and at 100% level is 400
+    // So the range of x2 is 300 (400 - 100)
+    // We map the level percentage (from 0 to 100) to this range (100 to 400)
+    return 100 + (levelPercentage / 100) * 300;
   }
 
   function calculateLevelPercentage(userXp, userLevel) {
     const initialXp = 1000;
     const growthFactor = 1.2;
 
-    let xpToNextLevel = initialXp * Math.pow(growthFactor, userLevel - 1);
-    let xpGainedTowardsNextLevel =
-      userXp - initialXp * Math.pow(growthFactor, userLevel - 2);
+    let xpToNextLevel = initialXp * Math.pow(growthFactor, userLevel);
 
-    return (xpGainedTowardsNextLevel / xpToNextLevel) * 100;
+    return (userXp / xpToNextLevel) * 100;
   }
 
   let levelPercentage = calculateLevelPercentage(userStats.xp, userStats.level);
